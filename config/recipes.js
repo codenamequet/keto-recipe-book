610833e4b3cabeadb9f54c49d0bb679c767ae517
Marquet Reid <you@example.com>
2018-04-03 22:14:29 -0400

    working on styling

--- config/recipes.js
+++ config/recipes.js
@@ -1,37 +1,41 @@
-const express = require("express")
-const Recipe = require("../db/models/schema.js")
+const express = require('express')
+const Recipe = require('../db/models/schema.js')
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    Recipe.find({})
-        .then(recipes => {
-            res.render("recipe-index", { recipes: recipes })
-        })
-        .catch(err => {
-            console.log(err)
-        })
+// router.get('/', (req, res) => {
+//     Recipe.find({})
+//         .then(recipes => {
+//             res.render('recipe-index', { recipes: recipes })
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+// })
+
+router.get('/recipes', req, res => {
+    res.render('recipe-index')
 })
 
-router.get("/add-recipe", (req, res) => {
-    res.render("add-recipe")
+router.get('/add-recipe', (req, res) => {
+    res.render('add-recipe')
 })
 
-router.get("/about-keto", (req, res) => {
-    res.render("about-keto")
+router.get('/about-keto', (req, res) => {
+    res.render('about-keto')
 })
 
-router.get("/:name", (req, res) => {
+router.get('/:name', (req, res) => {
     Recipe.findOne({ name: req.params.name })
         .then(recipe => {
-            res.render("single-recipe", { recipe: recipe })
+            res.render('single-recipe', { recipe: recipe })
         })
         .catch(err => {
             console.log(err)
         })
 })
 
-router.post("/add-recipe", (req, res) => {
-    console.log("I'm in the router")
+router.post('/add-recipe', (req, res) => {
+    console.log('I'm in the router')
     Recipe.create(req.body.recipe)
         .then(recipe => {
             res.redirect(`/recipes/${recipe.name}`)
@@ -41,9 +45,9 @@ router.post("/add-recipe", (req, res) => {
         })
 })
 
-router.put("/:name", (req, res) => {
-    console.log(req.body.recipe.ingredients.split(","))
-    req.body.recipe.ingredients.split(",")
+router.put('/:name', (req, res) => {
+    console.log(req.body.recipe.ingredients.split(','))
+    req.body.recipe.ingredients.split(',')
     Recipe.findOneAndUpdate({ name: req.params.name }, req.body.recipe, {
         new: true
     })
@@ -55,7 +59,7 @@ router.put("/:name", (req, res) => {
         })
 })
 
-router.delete("/:name", (req, res) => {
+router.delete('/:name', (req, res) => {
     Recipe.findOne({ name: req.params.name })
         .remove()
         .then(() => {
